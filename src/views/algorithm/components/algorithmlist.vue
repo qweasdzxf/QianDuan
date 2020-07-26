b8053c325e4e44718003f4dc833c2b52a40ea890
Aezqsss <823439378@qq.com>
2020-07-26 10:03:11 +0800

    解决list为空 报错 v2

--- src/views/algorithm/components/algorithmlist.vue
+++ src/views/algorithm/components/algorithmlist.vue
@@ -9,7 +9,7 @@
                 <el-col :span="6">
                   <el-button type="info">显示帮助</el-button>
                 </el-col>
-                <el-col :span="5" :offset="12">
+                <el-col :span="6" :offset="6">
                   <el-input v-model="keyWord" placeholder="请输入内容">
                     <el-button
                       slot="append"
@@ -22,11 +22,11 @@
               </el-row>
             </el-card>
           </el-header>
-          <br />
+          <br>
           <el-main>
             <div>
               <el-card shadow="hover">
-                <el-table :data="pageInfo.list" style="width: 100%">
+                <el-table v-if="pageInfo!=null" :data="pageInfo.list" style="width: 100%">
                   <el-table-column prop="algorithmId" label="算法ID" style="font-size: large" />
                   <el-table-column prop="algorithmName" label="算法名称" />
                   <el-table-column prop="algorithmType.algorithmTypeName" label="算法类别" />
@@ -55,35 +55,35 @@
                   </el-table-column>
                 </el-table>
               </el-card>
-              <el-dialog title="查看算法" :visible.sync="showAlgorithmDialog" center="true">
+              <el-dialog title="查看算法" :visible.sync="showAlgorithmDialog" :center="true">
                 <el-container>
                   <el-header>
                     <div>
                       <el-row>
-                        <el-col span="8">算法名称：{{ algorithmDetail.algorithmName }}</el-col>
-                        <el-col span="16">算法状态：{{ algorithmDetail.algorithmStatus }}</el-col>
+                        <el-col :span="8">算法名称：{{ algorithmDetail.algorithmName }}</el-col>
+                        <el-col :span="8">算法状态：{{ algorithmDetail.algorithmStatus }}</el-col>
                       </el-row>
-                      <br />
+                      <br>
                       <el-row>
-                        <el-col span="8">算法版本：{{ algorithmDetail.algorithmVersion }}</el-col>
-                        <el-col span="16">创建时间：{{ algorithmDetail.algorithmCreateTime }}</el-col>
+                        <el-col :span="8">算法版本：{{ algorithmDetail.algorithmVersion }}</el-col>
+                        <el-col :span="16">创建时间：{{ algorithmDetail.algorithmCreateTime }}</el-col>
                       </el-row>
-                      <br />
+                      <br>
                       <el-row>
-                        <el-col span="8">算法描述：{{ algorithmDetail.algorithmDescription }}</el-col>
+                        <el-col :span="8">算法描述：{{ algorithmDetail.algorithmDescription }}</el-col>
                       </el-row>
-                      <br />
+                      <br>
                     </div>
                   </el-header>
                   <el-main>
-                    <br />
+                    <br>
                     <el-tabs v-model="activeName" @tab-click="handleClick">
                       <el-tab-pane label="训练规范" name="train">
                         <h4>配置信息</h4>
                         <el-row>训练框架：{{ algorithmDetail.aiEngine.algorithmEngineName + algorithmDetail.aiEngine.algorithmEngineVersion + '-' + algorithmDetail.aiEngine.pythonVersionName }}</el-row>
-                        <br />
+                        <br>
                         <el-row>代码路径：{{ algorithmDetail.algorithmSaveUrl }}</el-row>
-                        <br />
+                        <br>
                         <el-row>启动文件：{{ algorithmDetail.algorithmStarterUrl }}</el-row>
                         <h4>推荐规格</h4>
                         <el-row>规格类型：{{ algorithmDetail.instanceType.instanceTypeDescription }}</el-row>
@@ -116,11 +116,10 @@
                   <div class="block">
                     <span class="demonstration">选择AI引擎</span>
                     <el-cascader
-                      v-model="value"
+                      v-model="engineValue"
                       :options="engineList"
                       :props="{ expandTrigger: 'hover' }"
                       @focus="getEngines"
-                      @change="handleChange"
                     />
                   </div>
                   <el-divider />
@@ -181,8 +180,8 @@
                 :current-page.sync="pageNum"
                 :page-size="pageSize"
                 layout="prev, pager, next, jumper"
+                v-if="pageInfo!=null"
                 :total="pageInfo.total"
-                @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
               />
             </div>
@@ -194,218 +193,290 @@
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios'
 export default {
-  name: "Algorithmlist",
+  name: 'Algorithmlist',
   data() {
     return {
+      engineValue: null,
       showPage: false,
       dialogFormVisible: false,
       showAlgorithmDialog: false,
-      activeName: "train",
+      activeName: 'train',
       pageNum: null,
       pageSize: 10,
       userId: null,
-      keyWord: "",
+      keyWord: '',
+      engineList: null,
       pageInfo: null,
       algorithmDetail: {
         algorithmId: 60,
-        algorithmName: "a s d",
-        algorithmVersion: "阿法",
+        algorithmName: 'a s d',
+        algorithmVersion: '阿法',
         algorithmTypeId: 1,
         algorithmType: {
           algorithmTypeId: 1,
-          algorithmTypeName: "图像分类",
-          algorithmTypeDescription: "暂时没有描述",
+          algorithmTypeName: '图像分类',
+          algorithmTypeDescription: '暂时没有描述'
         },
         algorithmEngineId: 3,
         aiEngine: {
           algorithmEngineId: 3,
-          algorithmEngineName: "PyTorch",
-          algorithmEngineVersion: "1.0.0",
-          pythonVersionName: "python2.7",
+          algorithmEngineName: 'PyTorch',
+          algorithmEngineVersion: '1.0.0',
+          pythonVersionName: 'python2.7'
         },
         algorithmDescriptionId: 62,
         algorithmDescription: null,
         algorithmInstanceTypeId: 1,
         instanceType: {
           instanceTypeId: 1,
-          instanceTypeDescription: "1核2G",
+          instanceTypeDescription: '1核2G'
         },
-        algorithmInputReflect: "",
-        algorithmOutputReflect: "",
-        algorithmStarterUrl: "/Users/thomas/Desktop/Data",
-        algorithmSaveUrl: "/Users/thomas/Desktop/Data",
+        algorithmInputReflect: '',
+        algorithmOutputReflect: '',
+        algorithmStarterUrl: '/Users/thomas/Desktop/Data',
+        algorithmSaveUrl: '/Users/thomas/Desktop/Data',
         algorithmCustomizeHyperPara: 1,
         algorithmStatus: 0,
-        algorithmCreateTime: "2020-07-17T09:49:21",
-        algorithmImageId: null,
+        algorithmCreateTime: '2020-07-17T09:49:21',
+        algorithmImageId: null
       },
       hyperParameters: [
         {
           hyperParaId: 11,
-          hyperParaName: "hyper parameters",
-          hyperParaDescription: "this is description",
+          hyperParaName: 'hyper parameters',
+          hyperParaDescription: 'this is description',
           hyperParaType: 0,
           hyperParaAllowAdjust: true,
-          hyperParaRange: "0-100",
-          hyperParaDefaultValue: "10",
+          hyperParaRange: '0-100',
+          hyperParaDefaultValue: '10',
           hyperParaIsNeeded: false,
-          algorithmId: 34,
+          algorithmId: 34
         },
         {
           hyperParaId: 12,
-          hyperParaName: "hyper parameters",
-          hyperParaDescription: "this is description",
+          hyperParaName: 'hyper parameters',
+          hyperParaDescription: 'this is description',
           hyperParaType: 0,
           hyperParaAllowAdjust: true,
-          hyperParaRange: "0-100",
-          hyperParaDefaultValue: "10",
+          hyperParaRange: '0-100',
+          hyperParaDefaultValue: '10',
           hyperParaIsNeeded: true,
-          algorithmId: 34,
-        },
+          algorithmId: 34
+        }
       ],
       form: {
-        name: "",
-        region: "",
-        date1: "",
-        date2: "",
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
         delivery: false,
         type: [],
-        resource: "",
-        desc: "",
+        resource: '',
+        desc: ''
       },
-      newData:{},
-      formLabelWidth: "120px",
-    };
+      newData: {},
+      formLabelWidth: '120px'
+    }
   },
   created() {
-    this.getAlgorithmList();
+    this.getAlgorithmList()
     // this.getParams();
   },
-  mounted() {},
+  mounted() {
+    this.getAlgorithmList()
+  },
   methods: {
-    getParams(){
-      console.log("getparams");
+    isCustomize() {
+      console.log('func is customize')
+    },
+    getParams() {
+      console.log('getparams')
       // this.id = self.$route.query.id;
       console.log(this.$route.query.newData)
-      this.newData.algorithmId=9999;
+      this.newData.algorithmId = 9999
       //  this.newData.algorithmName=this.$route.query.newData.algorithm_name;
-      this.newData.algorithmName='face';
-       if(this.$route.query.newData.algorithm_type_id==1){
-        this.newData.algorithmType='图形分类'
-       }else{
-          this.newData.algorithmType='物体检测'
-       }
-       
+      this.newData.algorithmName = 'face'
+      if (this.$route.query.newData.algorithm_type_id == 1) {
+        this.newData.algorithmType = '图形分类'
+      } else {
+        this.newData.algorithmType = '物体检测'
+      }
+
       //  this.newData.algorithmVersion=this.$route.query.newData.algorithm_version;
-      this.newData.algorithmType={algorithmTypeName:'物体检测'};
-      this.newData.algorithmVersion='v1.0';
-       this.newData.algorithmCreateTime='2020-07-25T14:00:40';
-       console.log("this.newData");
-       console.log(this.newData);
-      this.pageInfo.list.push(this.newData);
-    }
-    ,
+      this.newData.algorithmType = { algorithmTypeName: '物体检测' }
+      this.newData.algorithmVersion = 'v1.0'
+      this.newData.algorithmCreateTime = '2020-07-25T14:00:40'
+      console.log('this.newData')
+      console.log(this.newData)
+      this.pageInfo.list.push(this.newData)
+    },
     deleteRow(index, rows) {
-      rows.splice(index, 1);
+      rows.splice(index, 1)
     },
     handleClick(row) {
-      console.log(row);
+      console.log(row)
     },
     searchWithKeyWord() {
-      this.pageNum = 1;
-      this.getAlgorithmList();
+      this.pageNum = 1
+      this.getAlgorithmList()
     },
     handleCurrentChange() {
-      this.getAlgorithmList();
+      this.getAlgorithmList()
+    },
+    getEngines() {
+      console.log('trying get engines!')
+      axios.get('/algorithm/frontstage/engines').then((response) => {
+        console.log(response.data['extend'])
+        var engines = response.data['extend'].engines
+        console.log(engines)
+        var engineList = []
+        for (var i = 0; i < engines.length; i++) {
+          var engine = engines[i]
+          var flag = true
+          // 先根据引擎名查找
+          engineList.forEach((value) => {
+            if (engine.algorithmEngineName == value.value) {
+              console.log('find!')
+              // 找到后查看children节点
+              var innerFlag = true
+              value.children.forEach((childValue) => {
+                if (childValue.value == engine.algorithmEngineVersion) {
+                  innerFlag = false
+                  childValue.children.push({
+                    value: engine.pythonVersionName,
+                    label: engine.pythonVersionName
+                  })
+                }
+              })
+              if (innerFlag) {
+                value.children.push({
+                  value: engine.algorithmEngineVersion,
+                  label:
+                    engine.algorithmEngineName + engine.algorithmEngineVersion,
+                  children: [
+                    {
+                      value: engine.pythonVersionName,
+                      label: engine.pythonVersionName
+                    }
+                  ]
+                })
+              }
+              flag = false
+            }
+          })
+          if (flag) {
+            engineList.push({
+              value: engine.algorithmEngineName,
+              label: engine.algorithmEngineName,
+              children: [
+                {
+                  value: engine.algorithmEngineVersion,
+                  label:
+                    engine.algorithmEngineName + engine.algorithmEngineVersion,
+                  children: [
+                    {
+                      value: engine.pythonVersionName,
+                      label: engine.pythonVersionName
+                    }
+                  ]
+                }
+              ]
+            })
+          }
+        }
+        console.log(engineList)
+        this.engineList = engineList
+        this.engines = engines
+      })
     },
     getAlgorithmList() {
-      console.log("trying get algorithms!");
+      console.log('trying get algorithms!')
       axios
-        .get("/algorithm/backstage/algorithms", {
+        .get('/algorithm/backstage/algorithms', {
           params: {
             pageNum: this.pageNum,
             pageSize: this.pageSize,
-            keyWord: this.keyWord,
-          },
+            keyWord: this.keyWord
+          }
         })
         .then((responese) => {
-          this.pageInfo = responese.data.extend.pageInfo;
-          if(this.pageNum==11){
-            this.getParams();
+          this.pageInfo = responese.data.extend.pageInfo
+          if (this.pageNum == 11) {
+            this.getParams()
           }
-          console.log(responese);
-        });
-      this.showPage = true;
+          console.log(responese)
+        })
+      this.showPage = true
     },
     closeShowAlgorithmDetail() {
-      console.log("close dialog");
-      this.showAlgorithmDialog = false;
-      console.log(this.showAlgorithmDialog);
+      console.log('close dialog')
+      this.showAlgorithmDialog = false
+      console.log(this.showAlgorithmDialog)
     },
-    createTrainTask(index,row){
-      console.log(row);
-       this.$router.push({
-        path: "../train/create",
-        query:{
-          data:row
+    createTrainTask(index, row) {
+      console.log(row)
+      this.$router.push({
+        path: '../train/create',
+        query: {
+          data: row
         }
-      });
+      })
     },
     showAlgorithmDetail(index) {
-      console.log(index);
-      console.log(this.pageInfo.list);
+      console.log(index)
+      console.log(this.pageInfo.list)
       axios
         .get(
-          "/frontstage/algorithm/" +
+          '/algorithm/frontstage/algorithm/' +
             this.pageInfo.list[index].algorithmId.toString()
         )
         .then((res) => {
-          this.algorithmDetail = res.data.extend.algorithm;
+          this.algorithmDetail = res.data.extend.algorithm
           switch (this.algorithmDetail.algorithmStatus) {
             case 0:
-              this.algorithmDetail.algorithmStatus = "就绪";
-              break;
+              this.algorithmDetail.algorithmStatus = '就绪'
+              break
             case 1:
-              this.algorithmDetail.algorithmStatus = "异常";
-              break;
+              this.algorithmDetail.algorithmStatus = '异常'
+              break
           }
-        });
+        })
       axios
         .get(
-          "/frontstage//algorithm/hyperPara/" +
+          '/algorithm/frontstage/algorithm/hyperPara/' +
             this.pageInfo.list[index].algorithmId.toString()
         )
         .then((res) => {
-          this.hyperParameters = res.data.extend.hyperParameters;
-          console.log(this.hyperParameters);
+          this.hyperParameters = res.data.extend.hyperParameters
+          console.log(this.hyperParameters)
           this.hyperParameters.forEach((value) => {
-            console.log(value);
+            console.log(value)
             switch (value.hyperParaType) {
               case 0:
-                value.hyperParaType = "Integer";
-                break;
+                value.hyperParaType = 'Integer'
+                break
               case 1:
-                value.hyperParaType = "Double";
-                break;
+                value.hyperParaType = 'Double'
+                break
               case 2:
-                value.hyperParaType = "Boolean";
-                break;
+                value.hyperParaType = 'Boolean'
+                break
               case 3:
-                value.hyperParaType = "String";
-                break;
+                value.hyperParaType = 'String'
+                break
             }
             value.hyperParaAllowAdjust = value.hyperParaAllowAdjust
-              ? "是"
-              : "否";
-            value.hyperParaIsNeeded = value.hyperParaIsNeeded ? "是" : "否";
-          });
-        });
-      this.showAlgorithmDialog = true;
-    },
-  },
-};
+              ? '是'
+              : '否'
+            value.hyperParaIsNeeded = value.hyperParaIsNeeded ? '是' : '否'
+          })
+        })
+      this.showAlgorithmDialog = true
+    }
+  }
+}
 </script>
 
 <style scoped>
